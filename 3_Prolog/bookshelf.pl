8198d8ec4825f6f158b5c72548e2b5c96eff14a2
tanete <tanete772@outlook.com>
2015-08-19 14:16:51 +0800

    3_Prolog_Day_1

---
+++ 3_Prolog/bookshelf.pl
@@ -0,0 +1,10 @@
+author_book('Kevin Kelly', 'Out of Control').
+author_book('Kevin Kelly', 'What Technology Wants').
+author_book('Kevin Kelly', 'Cool Tools: A Catalog of Possibilities')
+author_book('Peter Thiel', 'Zero to One: Notes on Startups, or How to Build the Future').
+author_book('Peter Thiel', 'The Diversity Myth').
+author_book('Wu Jun', 'Road to the University').
+author_book('Wu Jun', 'On Top of Tides').
+author_book('Wu Jun', 'Beauty of Mathematics').
+
+% author_book('Kevin Kelly', Books).
---
+++ 3_Prolog/music.pl
@@ -0,0 +1,10 @@
+musician_instr('Foryo', 'Guitar').
+musician_instr('Foryo', 'Piano').
+musician_instr('Jacva Bias', 'Guitar').
+musician_instr('Song ZY', 'Soxs').
+musician_instr('LLD', 'Basi').
+
+musician_genre('Foryo', 'EDM').
+musician_genre('LLD', 'Pop').
+
+% musician_instr(Who, 'Guitar').
